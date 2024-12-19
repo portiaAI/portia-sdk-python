f6beee661e642a50342a430d45536e31c9c5eb3b
Tom Stuart <tom@portialabs.ai>
2024-12-19 16:40:50 +0000

    plan/query

--- portia/cli.py
+++ portia/cli.py
@@ -8,9 +8,14 @@ from portia.tool_registry import InMemoryToolRegistry
 from tests.utils import AdditionTool
 
 
+@click.group()
+def cli() -> None:
+    """Portia CLI."""
+
+
 @click.command()
 @click.argument("query")
-def cli(query: str) -> None:
+def run(query: str) -> None:
     """Run a query."""
     config = Config()
     tool_registry = InMemoryToolRegistry.from_local_tools([AdditionTool()])
@@ -18,6 +23,20 @@ def cli(query: str) -> None:
     output = runner.run_query(query, tools=[])
     click.echo(output)
 
+
+@click.command()
+@click.argument("query")
+def plan(query: str) -> None:
+    """Plan a query."""
+    config = Config()
+    tool_registry = InMemoryToolRegistry.from_local_tools([AdditionTool()])
+    runner = Runner(config=config, tool_registry=tool_registry)
+    output = runner.plan_query(query, tools=[])
+    click.echo(output)
+
+
+cli.add_command(run)
+cli.add_command(plan)
 
 if __name__ == "__main__":
     cli()
