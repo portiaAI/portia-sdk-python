3e04598376d263b4f24724d29ff5c27ee32f6b27
Tom Stuart <tom@portialabs.ai>
2024-12-23 10:43:16 +0000

    cli

--- portia/cli.py
+++ portia/cli.py
@@ -8,7 +8,7 @@ portia-cli plan "add 4 + 8" - plan a query
 
 import click
 
-from portia.config import Config
+from portia.config import default_config
 from portia.runner import Runner
 from portia.tool_registry import InMemoryToolRegistry
 from tests.utils import AdditionTool
@@ -23,7 +23,7 @@ def cli() -> None:
 @click.argument("query")
 def run(query: str) -> None:
     """Run a query."""
-    config = Config()
+    config = default_config()
     tool_registry = InMemoryToolRegistry.from_local_tools([AdditionTool()])
     runner = Runner(config=config, tool_registry=tool_registry)
     output = runner.run_query(query, tools=[])
@@ -34,7 +34,7 @@ def run(query: str) -> None:
 @click.argument("query")
 def plan(query: str) -> None:
     """Plan a query."""
-    config = Config()
+    config = default_config()
     tool_registry = InMemoryToolRegistry.from_local_tools([AdditionTool()])
     runner = Runner(config=config, tool_registry=tool_registry)
     output = runner.plan_query(query, tools=[])
