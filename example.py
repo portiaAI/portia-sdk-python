5ea01fef6f216c27182928b0eb85a578ce6e807e
Omar Elmohandes <omar@portialabs.ai>
2025-01-10 16:09:13 +0000

    Fixing the ruff

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 """Simple Example."""
 
-from portia.config import Config, LogLevel
+from portia.config import Config
 from portia.example_tools.registry import example_tool_registry
 from portia.runner import Runner
 
