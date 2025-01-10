84f89273959d18364b133b5017be59af03f53747
Tom Stuart <tom@portialabs.ai>
2025-01-10 09:19:12 +0000

    example

--- example.py
+++ example.py
@@ -1,11 +1,10 @@
 """Simple Example."""
 
-from uuid import UUID
-
 from portia.config import Config, LogLevel
 from portia.context import execution_context
 from portia.example_tools.registry import example_tool_registry
 from portia.runner import Runner
+from portia.workflow import WorkflowState
 
 runner = Runner(
     Config.from_default(default_log_level=LogLevel.DEBUG),
@@ -26,11 +25,10 @@ with execution_context(end_user_id="123"):
 
 
 # Resuming after a clarification
-workflow_id = UUID("12cc58ae-bf1d-4a96-9680-ec9e0cb564e5")
-workflow = runner.storage.get_workflow(workflow_id)
+workflow = runner.storage.get_workflow(workflow.id)
+if workflow.state == WorkflowState.NEED_CLARIFICATION:
+    for c in workflow.get_outstanding_clarifications():
+        c.resolve(response=None)
 
-for c in workflow.get_outstanding_clarifications():
-    c.resolve(response=None)
-
-with execution_context(context=workflow.execution_context):
-    runner.execute_workflow(workflow)
+    with execution_context(context=workflow.execution_context):
+        runner.execute_workflow(workflow)
