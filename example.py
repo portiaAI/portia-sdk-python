ba13600a312a34abf2b74cdeedcb93b41eade40a
Tom Stuart <tom@portialabs.ai>
2025-01-14 12:03:07 +0000

    update

--- example.py
+++ example.py
@@ -21,17 +21,20 @@ runner.execute_workflow(workflow)
 
 # With Execution Context
 with execution_context(end_user_id="123"):
-    plan = runner.create_workflow_from_query(
+    workflow = runner.create_workflow_from_query(
         "Get the temperature in London and Sydney and then add the two temperatures rounded to 2DP",
     )
-    workflow = runner.execute_workflow(plan)
+    workflow = runner.execute_workflow(workflow)
 
 
 # When we hit a clarification we can ask our end user for clarification then resume the process
 with execution_context(end_user_id="123", additional_data={"email_address": "hello@portialabs.ai"}):
-    workflow = runner.run_query(
+    workflow = runner.create_workflow_from_query(
         "Get the temperature in London and Sydney and then add the two temperatures rounded to 2DP",
     )
+    workflow = runner.execute_workflow(workflow)
+
+
 # Fetch workflow
 workflow = runner.storage.get_workflow(workflow.id)
 # Update clarifications
