3acfefc31f9200cf0cce26e56b1dc56397f24d72
Tom Stuart <tom@portialabs.ai>
2025-01-30 10:44:19 +0000

    hack_tom

--- portia/open_source_tools/registry.py
+++ portia/open_source_tools/registry.py
@@ -6,9 +6,5 @@ from portia.open_source_tools.weather import WeatherTool
 from portia.tool_registry import InMemoryToolRegistry
 
 example_tool_registry = InMemoryToolRegistry.from_local_tools(
-    [
-        CalculatorTool(),
-        WeatherTool(),
-        SearchTool(),
-    ],
+    [CalculatorTool(), WeatherTool(), SearchTool()],
 )
