e9713bb2212f5312a370c775e9460d111a4b5919
robbie-portia <robbie@portialabs.ai>
2025-05-12 13:35:54 +0100

    Update one shot agent (#400)
    * One shot agent test
    
    * Tidy
    
    * Ready for review
    
    * Sort linting
    
    * Update test
    
    * Clarification tool prompt update
    
    * Make argument name clearer

--- portia/execution_agents/one_shot_agent.py
+++ portia/execution_agents/one_shot_agent.py
@@ -10,20 +10,25 @@ from __future__ import annotations  # noqa: I001
 
 from typing import TYPE_CHECKING, Any
 
-from langchain_core.messages import SystemMessage
 from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 from langgraph.graph import END, START, MessagesState, StateGraph
-
 from langgraph.prebuilt import ToolNode
+from langchain_core.prompts import (
+    SystemMessagePromptTemplate,
+)
+
 from portia.errors import InvalidAgentError
+
 from portia.execution_agents.base_execution_agent import BaseExecutionAgent
 from portia.execution_agents.execution_utils import (
     AgentNode,
+    template_in_required_inputs,
     process_output,
     tool_call_or_end,
 )
 from portia.execution_agents.memory_extraction import MemoryExtractionStep
 from portia.execution_agents.utils.step_summarizer import StepSummarizer
+from portia.execution_agents.clarification_tool import ClarificationTool
 from portia.tool import ToolRunContext
 from portia.execution_agents.context import StepInput  # noqa: TC001
 from portia.plan import Step, ReadOnlyStep
@@ -71,23 +76,55 @@ class OneShotToolCallingModel:
 
     """
 
-    tool_calling_prompt = ChatPromptTemplate.from_messages(
+    arg_parser_prompt = ChatPromptTemplate.from_messages(
         [
-            SystemMessage(
-                content="You are a very powerful assistant, but don't know current events.",
+            SystemMessagePromptTemplate.from_template(
+                "You are a highly capable assistant tasked with calling tools based on the "
+                "provided inputs. "
+                "While you are not aware of current events, you excel at reasoning "
+                "and adhering to instructions. "
+                "Avoid assumptions or fabricated information. "
+                "If you are unsure of an argument to use for the tool, you can use the "
+                "clarification tool to clarify what the argument should be. "
+                "If any of the inputs is a large string and you want to use it verbatim, rather "
+                "than repeating it, you should provide the name in curly braces to the tool call"
+                " and it will be templated in before the tool is called. "
+                "For example, if you wish to use an input called '$large_input_value' verbatim, "
+                "you should enter '{{ '{{' }}$large_input_value{{ '}}' }}' (double curly braces "
+                "and include the $ in the name) and the value will be templated in before the tool "
+                "is called.  You should definitely use this templating for any input values over "
+                "1000 words that you want to use verbatim.",
+                # Use jinja2 to allow for the literal curly braces
+                template_format="jinja2",
             ),
             HumanMessagePromptTemplate.from_template(
-                [
-                    "query:",
-                    "{query}",
-                    "context:",
-                    "{context}",
-                    "Use the provided tool. You should provide arguments that match the tool's "
-                    "schema using the information contained in the query and context."
-                    "Important! Make sure to take into account previous clarifications in the "
-                    "context which are from the user and may change the query"
-                    "Make sure you don't repeat past errors: {past_errors}",
-                ],
+                "Context for user input and past steps:\n{context}\n"
+                "Task: {task}\n"
+                "The system has a tool available named '{tool_name}'.\n"
+                "Argument schema for the tool:\n{tool_args}\n"
+                "Description of the tool: {tool_description}\n"
+                "You also have a clarification tool available with argument schema:"
+                "\n{clarification_tool_args}\n"
+                "\n\n----------\n\n"
+                "The following section contains previous errors. "
+                "Ensure your response avoids these errors. "
+                "The one exception to this is not providing a value for a required argument. "
+                "If a value cannot be extracted from the context, you can leave it blank. "
+                "Do not assume a default value that meets the type expectation or is a common testing value. "  # noqa: E501
+                "Here are the previous errors:\n"
+                "{previous_errors}\n"
+                "\n\n----------\n\n"
+                "Please call the tool to achieve the above task, following the guidelines below:\n"
+                "- If a tool needs to be called many times, you can repeat the argument\n"
+                "- You may take values from the task, inputs, previous steps or clarifications\n"
+                "- Prefer values clarified in follow-up inputs over initial inputs.\n"
+                "- Do not provide placeholder values (e.g., 'example@example.com').\n"
+                "- Do not include references to any of the input values (e.g. 'as provided in "
+                "the input'): you must put the exact value the tool should be called with in "
+                "the value field\n"
+                "- Ensure arguments align with the tool's schema and intended use."
+                "- If you are unsure of an argument to use for the tool, you can use the "
+                "clarification tool to clarify what the argument should be.\n\n"
             ),
         ],
     )
@@ -126,17 +163,25 @@ class OneShotToolCallingModel:
             dict[str, Any]: A dictionary containing the model's generated response.
 
         """
+        if not self.agent.tool:
+            raise InvalidAgentError("Parser model has no tool")
+
         model = self.model.to_langchain().bind_tools(self.tools)
         messages = state["messages"]
-        past_errors = [msg for msg in messages if "ToolSoftError" in msg.content]
-        context = self.agent.get_system_context(self.tool_context, state["step_inputs"])
-        response = model.invoke(
-            self.tool_calling_prompt.format_messages(
-                query=self.agent.step.task,
-                context=context,
-                past_errors=past_errors,
-            ),
+        past_errors = [str(msg) for msg in messages if "ToolSoftError" in msg.content]
+        clarification_tool = ClarificationTool(step=self.agent.plan_run.current_step_index)
+        formatted_messages = self.arg_parser_prompt.format_messages(
+            context=self.agent.get_system_context(self.tool_context, state["step_inputs"]),
+            task=self.agent.step.task,
+            tool_name=self.agent.tool.name,
+            tool_args=self.agent.tool.args_json_schema(),
+            tool_description=self.agent.tool.description,
+            clarification_tool_args=clarification_tool.args_json_schema(),
+            previous_errors=",".join(past_errors),
         )
+        response = model.invoke(formatted_messages)
+        result = template_in_required_inputs(response, state["step_inputs"])
+
         if (
             self.agent.execution_hooks
             and self.agent.execution_hooks.before_tool_call
@@ -152,8 +197,7 @@ class OneShotToolCallingModel:
                 if clarification:
                     self.agent.new_clarifications.append(clarification)
                     return {"messages": []}
-
-        return {"messages": [response]}
+        return {"messages": [result]}
 
 
 class OneShotAgent(BaseExecutionAgent):
@@ -213,10 +257,12 @@ class OneShotAgent(BaseExecutionAgent):
         )
 
         model = self.config.get_execution_model()
+        clarification_tool = ClarificationTool(step=self.plan_run.current_step_index)
         tools = [
             self.tool.to_langchain_with_artifact(
                 ctx=tool_run_ctx,
             ),
+            clarification_tool.to_langchain_with_artifact(ctx=tool_run_ctx),
         ]
         tool_node = ToolNode(tools)
 
