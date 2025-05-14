cce8a713ba19498e546d4e03a370075dfbed2582
Tom Stuart <tom@portialabs.ai>
2025-05-14 14:00:47 +0100

    Add remove additional data

--- tests/unit/test_end_user.py
+++ tests/unit/test_end_user.py
@@ -9,5 +9,7 @@ def test_portia_local_default_config_with_api_keys() -> None:
 
     end_user.set_additional_data("other", "value")
     assert end_user.get_additional_data("other") == "value"
+    assert end_user.remove_additional_data("other")
+    assert end_user.get_additional_data("other") is None
 
     assert end_user.get_additional_data("not_set") is None
